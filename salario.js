2edaf3626e066db7dd1732876407b1f16a8e727e
Mauricio Selfene <m.selfene@gmail.com>
2021-07-09 02:23:29 -0400

    Ingreso de analisis y mediana de sueldo

---
+++ salario.js
@@ -0,0 +1,70 @@
+const chile = [];
+
+chile.push({
+    name: "Mauricio",
+    salary: 1000 
+});
+chile.push({
+    name: "Pepito",
+    salary: 1000 
+});
+chile.push({
+    name: "Ramon",
+    salary: 1000 
+});
+chile.push({
+    name: "Jadue",
+    salary: 1000 
+});
+chile.push({
+    name: "TODOs",
+    salary: 1000 
+});
+chile.push({
+    name: "asdas",
+    salary: 1000 
+});
+chile.push({
+    name: "asdsad",
+    salary: 1000 
+});
+chile.push({
+    name: "asdsddd",
+    salary: 1000 
+});
+chile.push({
+    name: "sdfgr",
+    salary: 1000 
+});
+chile.push({
+    name: "erferthjytrh",
+    salary: 1000 
+});
+chile.push({
+    name: "fegertfg",
+    salary: 1000 
+});
+chile.push({
+    name: "rghntry",
+    salary: 1000 
+});
+chile.push({
+    name: "bgnt5yhn",
+    salary: 1000 
+});
+chile.push({
+    name: "Mauricrgnbhio",
+    salary: 1000 
+});
+chile.push({
+    name: "fvberfv",
+    salary: 1000 
+});
+chile.push({
+    name: "efgbhrgtb",
+    salary: 1000 
+});
+chile.push({
+    name: "efgbghyn",
+    salary: 1000 
+});
